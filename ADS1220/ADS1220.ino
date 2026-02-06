47a35d315baba00c975bfec7458d6ef3fd50a98f
Wenpeng Wang <wwang11@wpi.edu>
2026-02-06 10:34:54 -0500

    reduce noise: 1) add median filter, 2) increase serial rate to 115200

--- ADS1220/ADS1220.ino
+++ ADS1220/ADS1220.ino
@@ -16,6 +16,7 @@ ADS1220_WE ads(ADS1220_CS_PIN, ADS1220_DRDY_PIN);
 #define NUM_SENSORS 2   // <-- USER SETS THIS (1–16)
 #define MA_WINDOW 100      // moving average window size
 #define EMA_ALPHA    0.1   // 0 < alpha <= 1
+#define MEDIAN_WIN   3
 
 
 float ma_buffer[NUM_SENSORS][MA_WINDOW];
@@ -23,8 +24,11 @@ uint8_t ma_index = 0;
 bool ma_filled = false;
 
 float ema_state[NUM_SENSORS];
-bool ema_initialized = false;
+bool ema_initialized[NUM_SENSORS] = {false};
 
+float median_buf[NUM_SENSORS][MEDIAN_WIN];
+uint8_t median_idx[NUM_SENSORS] = {0};
+bool median_initialized[NUM_SENSORS] = {false};
 
 
 
@@ -66,8 +70,9 @@ float movingAverage(uint8_t ch, float new_value) {
 }
 
 float emaFilter(uint8_t ch, float x) {
-  if (!ema_initialized) {
-    ema_state[ch] = x;   // initialize without jump
+  if (!ema_initialized[ch]) {
+    ema_state[ch] = x;          // initialize per channel
+    ema_initialized[ch] = true;
     return x;
   }
 
@@ -75,10 +80,53 @@ float emaFilter(uint8_t ch, float x) {
   return ema_state[ch];
 }
 
+inline float median3(float a, float b, float c) {
+  if (a > b) {
+    float t = a;
+    a = b;
+    b = t;
+  }
+  if (b > c) {
+    float t = b;
+    b = c;
+    c = t;
+  }
+  if (a > b) {
+    float t = a;
+    a = b;
+    b = t;
+  }
+  return b;
+}
+
+float medianFilter(uint8_t ch, float x) {
+
+  // --- Initialize median buffer with first real sample ---
+  if (!median_initialized[ch]) {
+    for (uint8_t i = 0; i < MEDIAN_WIN; i++) {
+      median_buf[ch][i] = x;
+    }
+    median_initialized[ch] = true;
+    return x;
+  }
+
+  median_buf[ch][median_idx[ch]] = x;
+  median_idx[ch] = (median_idx[ch] + 1) % MEDIAN_WIN;
+
+  return median3(
+    median_buf[ch][0],
+    median_buf[ch][1],
+    median_buf[ch][2]
+  );
+}
+
+
+
+
 
 /* ------------------- Setup ------------------- */
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   delay(200);
 
   /* MUX pins */
@@ -89,31 +137,31 @@ void setup() {
 
   /* Init SPI + ADS1220 */
   SPI.begin();
-  if(!ads.init()){
+  if (!ads.init()) {
     Serial.println("ADS1220 is not connected!");
-    while(1);
+    while (1);
   }
 
-  // ads.bypassPGA(false);                      // disable PGA for safety  
+  // ads.bypassPGA(false);                      // disable PGA for safety
   /* ADS1220 configuration */
- 
+
   ads.setCompareChannels(ADS1220_MUX_1_2);
   ads.setVRefSource(ADS1220_VREF_REFP1_REFN1);
   ads.setVRefValue_V(5.0);
   ads.setGain(ADS1220_GAIN_8);              // gain = 1
-  ads.setDataRate(ADS1220_DR_LVL_5);        // ~90 SPS
+  ads.setDataRate(ADS1220_DR_LVL_5);        // ~20 SPS
   ads.setOperatingMode(ADS1220_TURBO_MODE);
   ads.setConversionMode(ADS1220_CONTINUOUS);
-  ads.setFIRFilter(ADS1220_NONE);
- 
+  ads.setFIRFilter(ADS1220_50HZ_60HZ);
+
 
   Serial.println("ADS1220 initialized.");
 
-  delay(500);
-  
-//   selectMux(0);
-//   delay(100);
-//  
+  delay(100);
+
+  //   selectMux(0);
+  //   delay(100);
+  //
 }
 
 /* ------------------- Main Loop ------------------- */
@@ -152,18 +200,18 @@ void loop() {  // Exponential Moving Average (LPF)
   for (uint8_t i = 0; i < NUM_SENSORS; i++) {
 
     selectMux(i);
-    delay(50);
+    delay(3);
 
     float mv = ads.getVoltage_mV();
     float R  = convertReadingtoResistance(mv);
 
-    float R_filt = emaFilter(i, R);
+    // ---- Median → EMA ----
+    float R_med  = medianFilter(i, R);
+    float R_filt = emaFilter(i, R_med);
 
     Serial.print(R_filt, 5);
     if (i < NUM_SENSORS - 1) Serial.print(",");
   }
 
   Serial.println();
-
-  ema_initialized = true;
 }
